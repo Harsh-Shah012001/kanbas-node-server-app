73dde1a8dca4ce69bb5e195291e308c989673aee
Harsh-Shah012001 <shah.harsh8@northeastern.edu>
2024-12-02 20:50:54 -0500

    Unenroll button

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,6 +1,17 @@
 import Database from "../Database/index.js";
 export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  enrollments.push({ _id: Date.now(), user: userId, course: courseId });
+    const { enrollments } = Database;
+    enrollments.push({
+        _id: Date.now().toString() + Math.floor(Math.random() * 1000).toString().padStart(3, '0'),
+        user: userId, course: courseId
+    });
+    console.log("Enrollment",enrollments)
 }
-
+export function unenrollUserFromCourse(userId, courseId) {  
+  const { enrollments } = Database;
+    const index = enrollments.findIndex((enrollment) =>{return enrollment.course === courseId && enrollment.user === userId});
+    if (index !== -1) {
+        enrollments.splice(index, 1);
+        return true;
+    }
+};
--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -1,13 +1,13 @@
 import * as EnrollmentsDao from "./dao.js";
 export default function EnrollmentRoutes(app) {
- app.post("/api/enrollments/unenroll", async (req, res) => {
+ app.post("/api/enrollments/unenroll",  (req, res) => {
     const {userId, courseId} = req.body;
-   const status = await EnrollmentsDao.unenrollUserFromCourse(userId, courseId);
+   const status = EnrollmentsDao.unenrollUserFromCourse(userId, courseId);
    res.send(status);
  });
- app.post("/api/enrollments/enroll", async (req, res) => {
+ app.put("/api/enrollments/enroll",  (req, res) => {
     const {userId, courseId} = req.body;
-    const status = await EnrollmentRoutes.enrollUserInCourse(userId, courseId);
+    const status = EnrollmentsDao.enrollUserInCourse(userId, courseId);
     res.send(status);
   });
 }
